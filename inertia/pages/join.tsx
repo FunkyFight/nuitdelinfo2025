ee4607c08d11c7bb81dfd66f366c0d3d89f3202a
Théo <theomag2006@gmail.com>
2025-12-05 00:59:49 +0100

    Pour Aurel

--- inertia/pages/join.tsx
+++ inertia/pages/join.tsx
@@ -1,15 +1,32 @@
+import { useEffect } from 'react';
+import {Cell, Wall, draw} from '../js/kahoot.js';
+
 export default function Join() {
+    useEffect(() => {
+        draw();
+    }, []);
+    
     return <>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        <div id="input_container" style = {{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
+        <input type="text" id="codeInput" placeholder="Enter room code" style ={{
+            borderWidth: 2,
+            borderRadius: "25px",
+            margin: "5px",
+        }}/>
+        </div>
+        <div id="maze" style = {{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
+        <canvas id="c1" style={{
+            backgroundColor: "lightgrey",
+        }}></canvas>
+        </div>
+        <p>here2</p>
     </>
 }
